9b194a29e33aee5d302f8c7984dca45344c9dff8
Dominik Honnef <dominikh@fork-bomb.org>
2014-11-11 10:56:21 +0100

    enforce vanity import path

--- console.go
+++ console.go
@@ -22,7 +22,7 @@
 // Portions of this documentation are modifications based on work
 // created and shared by Google and used according to terms described
 // in the Creative Commons 3.0 Attribution License.
-package console
+package console // import "honnef.co/go/js/console"
 
 import (
 	"bytes"
